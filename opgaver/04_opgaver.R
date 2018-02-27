d4aa2f150b87675afe561d30088bf4dbaf82e3c7
Mathias Knudsen <mathiasknud@gmail.com>
2018-02-27 16:44:19 +0100

    Opgave 2 løst
    Simpel sum-funktion

--- opgaver/04_opgaver.R
+++ opgaver/04_opgaver.R
@@ -24,6 +24,11 @@ seats <- read_csv("https://raw.githubusercontent.com/jespersvejgaard/PDS/master/
 # 1. Eksekvér præamblen ovenfor 
 # Preamble er kørt
 # 2. Skriv en funktion, som tager to vektorer som argumenter, summerer dem og returnerer resultatet. 
+sum_result <- function(x,y){
+  print(x+y)
+}
+sum_result(5,2)
+
 # 3. Skriv en funktion, som tager en vektor som argument, og som beregner standardafvigelsen hvis n > 30, og ellers printer "N er under 30!" og afbryder (hint: `break`). Test med vektorerne `x`, `y`, `z` ovenfor.  
 # 4. Skriv et for-loop, som looper igennem alle kolonnerne i dataframen `seats` og beregner partiernes gennemsnitlige antal mandater
 # 5. Brug `lapply()`, `sapply()`, `vapply()` eller `map_dbl()` til at beregne det samme
